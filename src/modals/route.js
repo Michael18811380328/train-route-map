9f46aad50ce10605e3d2749383aa2aa12fe51ace
Michael An <1822852997@qq.com>
2025-07-21 17:55:22 +0800

    add

--- src/modals/route.js
+++ src/modals/route.js
@@ -1,11 +1,11 @@
 class Route {
   constructor(obj) {
-    this.id = obj.id;
-    this.name = obj.name;
-    this.color = obj.color || 'black';
-    this.speed = obj.speed || 100;
-    this.line_number = obj.line_number;
-    this.points = obj.points;
+    this.id = obj.id; // 路线ID，唯一
+    this.name = obj.name; // 路线名称
+    this.color = obj.color || 'black'; // 路线颜色，表示路线等级，默认为黑色
+    this.speed = obj.speed || 100; // 路线速度，单位为km/h，默认为100
+    this.line_number = obj.line_number; // 路线个数，表示单线或者复现，范围 1-4，默认为1
+    this.points = obj.points; // 路线经过的站点，每一项是 Station 对象的 ID 数组
   }
 }
 
--- src/modals/train.js
+++ src/modals/train.js
@@ -1,12 +1,12 @@
 class Train {
   constructor(obj) {
-    this.id = obj.id;
-    this.train_number = obj.train_number;
-    this.start = obj.start;
-    this.end = obj.end;
-    this.color = obj.color;
-    this.times = obj.times;
-    this.stations = obj.stations;
+    this.id = obj.id; // 火车ID，唯一
+    this.train_number = obj.train_number; // 火车编号，唯一
+    this.start = obj.start; // 火车起点站ID
+    this.end = obj.end; // 火车终点站ID
+    this.color = obj.color; // 火车颜色，表示火车等级，默认为黑色
+    this.times = obj.times; // 火车运行时刻表数组，单位分钟，0 表示 0:00，例如 [480, 485] 表示火车在 8:00、8:05 分别到达各个站点
+    this.stations = obj.stations; // 火车经过的站点 id 数组，每一项是 Station 对象的 ID 数组
   }
 }
 
